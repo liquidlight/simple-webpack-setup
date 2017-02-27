9d3b8a48d92e778e6480ffd22685872c207e3492
Joao Augusto <joao@liquidlight.co.uk>
2017-02-27 18:01:56 +0000

    Update project details and add Readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Webpack 2 + Vue 2.0
+
+Simple webpack setup for Vue js apps.
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,7 +1,3 @@
 
 import './modules/one';
 import './modules/three';
-
-document.addEventListener(x => {
-	console.log('he');
-});
